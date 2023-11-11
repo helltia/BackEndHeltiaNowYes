8b04d56ccf296af5f69e6c43a35445c5ac61546e
Luis Daniel Aldana de la Rosa <ldaldana001@gmail.com>
2023-11-11 17:35:57 -0600

    removed password encription

--- models/user-model.js
+++ models/user-model.js
@@ -40,48 +40,13 @@ const UserSchema = new mongoose.Schema(
             required: true,
             default: " "
         },
-        messages: {
+        message: {
             type: [MessageSchema],
             required: true,
             default: []
         }
     }
 )
-
-UserSchema.pre("save", function (next) {
-    const user = this
-
-    if (this.isModified("password") || this.isNew) {
-        bcrypt.genSalt(10, function (saltError, salt) {
-            if (saltError) {
-                return next(saltError)
-            } else {
-                bcrypt.hash(user.password, salt, function(hashError, hash) {
-                    if (hashError) {
-                        return next(hashError)
-                    }
-
-                    user.password = hash
-                    next()
-                })
-            }
-        })
-    } else {
-        return next()
-    }
-})
-
-UserSchema.methods.comparePassword = function(password, callback) {
-    bcrypt.compare(password, this.password, function(error, isMatch) {
-        if (error) {
-            return callback(error)
-        } else {
-            callback(null, isMatch)
-        }
-    })
-}
-
-
 
 const User = mongoose.model('User',UserSchema);
 
