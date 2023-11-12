1e7c3747cbbd0c49b52071e3d90047ca30991161
Luis Daniel Aldana de la Rosa <ldaldana001@gmail.com>
2023-11-11 19:36:05 -0600

    Added response for error

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -35,12 +35,10 @@ async function login(req, res){
         if(user) {
             res.status(200).json({
                 message: "Success",
-                obj: user
             })
         } else {
             res.status(401).json({
                 message: "Wrong user or password",
-                obj: null
             })
         }
     } catch (e) {
